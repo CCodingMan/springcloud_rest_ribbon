c50c0fe703c0f9afb5a2dcfddd97ad9ae0e278eb
CCodingMan <2693986772@qq.com>
2019-08-20 17:15:17 +0800

    加入hystrix断路器

--- service-ribbon/src/main/java/com/ljj/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/ljj/serviceribbon/ServiceRibbonApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+//@EnableDiscoveryClient // 可以是其他注册中心
 @SpringBootApplication
 @EnableEurekaClient // 只适用于Eureka作为注册中心
-//@EnableDiscoveryClient // 可以是其他注册中心
+@EnableHystrix
 public class ServiceRibbonApplication {
 
     public static void main(String[] args) {
--- service-ribbon/src/main/java/com/ljj/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/ljj/serviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.ljj.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +15,12 @@ public class HelloService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "helloError")
     public String helloService(String name){
         return restTemplate.getForObject("http://eureka-client/hello/" + name, String.class);
+    }
+
+    public String helloError(String name){
+        return "hello" + name + "sorry,error";
     }
 }
